4b9f3e3f610bb5f923c60fbbdb94a9f449eb8a7b
stwestphal <s.westphal@doxter.de>
2016-12-07 11:16:23 +0100

    dashboard tabellen

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,14 +3,47 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    section "Authors", :priority =>1 do
+      table_for Author.order("id desc").limit(10) do
+        column :id
+        column "Name", :last_name do |name|
+          link_to name.last_name, [:admin, name]
+        end
+        column "Vorname", :first_name
+        column :created_at
+
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
+    section "Genres", :priority =>1 do
+      table_for Genre.order("id desc").limit(10) do
+        column :id
+        column "Type", :name do |name|
+          link_to name.name, [:admin, name]
+        end
+        column :created_at
+
+      end
+    end
+
+    section "Books", :priority =>1 do
+      table_for Book.order("id desc").limit(10) do
+        column :id
+        column "Name", :name do |name|
+          link_to name.name, [:admin, name]
+        end
+        column :author
+        column :price
+        column :created_at
+
+      end
+    end
+      # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
     #   column do
